e5b88465c4ac2a1190c3983eecad65d46639fd5a
Julien <julien.lagneau10101@gmail.com>
2026-01-30 14:56:29 +0100

    Update password_checker.py

--- 01-password-security/password_checker.py
+++ 01-password-security/password_checker.py
@@ -1,1 +1,38 @@
+# Password Security Checker
+# Projet cybersécurité - niveau débutant
+
+import re
+
+password = input("Entre un mot de passe : ")
+
+score = 0
+
+# Longueur
+if len(password) >= 8:
+    score += 1
+
+# Majuscule
+if re.search(r"[A-Z]", password):
+    score += 1
+
+# Minuscule
+if re.search(r"[a-z]", password):
+    score += 1
+
+# Chiffre
+if re.search(r"[0-9]", password):
+    score += 1
+
+# Caractère spécial
+if re.search(r"[!@#$%^&*(),.?\":{}|<>]", password):
+    score += 1
+
+print("\nRésultat :")
+
+if score <= 2:
+    print("Mot de passe FAIBLE ❌")
+elif score == 3 or score == 4:
+    print("Mot de passe MOYEN ⚠️")
+else:
+    print("Mot de passe FORT ✅")
 
